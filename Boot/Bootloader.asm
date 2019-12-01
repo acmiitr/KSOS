36adc21c69177fd202d728841b9cc2bd5ff76857
= <=>
2019-12-01 13:36:47 +0530

    Working 16 bit read function

--- Boot/Bootloader.asm
+++ Boot/Bootloader.asm
@@ -1,54 +1,85 @@
 [org 0x7c00]
 [bits 16]
-mov bp, 0x9000
+mov bp,0x9000
 mov sp,bp
 
-mov si,Message ;Welcome message
-call print_si
-
-cli
-lgdt [gdt_descriptor]
-
-mov eax, cr0
-or eax,1
-mov cr0,eax
-
-jmp 0x08:pipeline_flush ;This is just to test out my understanding
-pipeline_flush:
-[bits 32]
-
-mov ebp, 0x90000
-mov esp,ebp
-
-mov ax, 0x10
-mov ds,ax
-mov es,ax
-mov fs,ax
-mov gs,ax
-mov ss,ax
-
-mov esi,Message
-call print_pm_esi
-
+call disk_read_16
+mov si,Message1
+call print_si_16
+mov si,Message2
+call print_si_16
 
 jmp $
 
 
-;Functions
+;
+;functions
+;
+
+disk_read_16:  ;we can make it user input next ... al probably
+	pusha
+		mov dl,0x80;Drive number
+		mov dh,0x0;This is actually head number/platter number
+		mov ch,0x0 ;This is track number
+		mov cl,2 ;This is sector number ... starts from 1
+		mov ah,0x02 ;interrupt function
+		mov al,1 ;Number of sectors to be read
+		mov bx,0x7c0
+		mov es,bx
+		mov bx,512
+		int 0x13
+	popa
+	ret
+print_si_16:
+        pusha 
+        mov ah,0x0e ;This is the higher byte of the ax register 
+        print_16_loop: 
+        ;       mov al,[si] 
+        ;       inc si 
+                lodsb ;Performs the function of the previous two shits 
+                cmp al,0 
+                je print_16_end 
+                int 0x10 
+                jmp print_16_loop 
+        print_16_end: 
+        mov al,0x0A ;Enter 
+        int 0x10 
+        mov al,0x0D ;Carriage return 
+        int 0x10 
+        popa 
+        ret
+[bits 32]  ;Need to understand the impact of this!!!
+print_esi_32:
+	pusha
+
+	mov ah,0xf0  ;White on black
+	mov ebx,0xb8000 ;Starting address
+
+	_print_pm_loop:
+		mov al,[esi]
+		mov [ebx],ax
+		add esi,1
+		add ebx,2
+		cmp al,0
+		je _print_pm_end
+	jmp _print_pm_loop
+	_print_pm_end:
+
+	popa
+	ret
 
 
-%include "functions.asm"
-%include "GDT.asm"
-
-
-
+;
 ;Data
-
-Message:
-db 'Welcome to your OS, Rishi is gay',0
-
-
-
-times 510 -( $ - $$ ) db 0
+;
+%include "GDT.asm"
+Message1: db 'Welcome to your OS',0
+times 510 - ($-$$) db 0
 dw 0xaa55
 
+;
+;Next sector - sector 2
+;
+Message2: db 'You are outside',0
+
+times 512 db 0
