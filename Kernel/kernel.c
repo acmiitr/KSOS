9670931df6cade522b1a00be8357d025e0a0f76e
= <=>
2019-12-02 11:59:20 +0530

    Kernel added

---
+++ Kernel/kernel.c
@@ -0,0 +1,20 @@
+#define VIDEO_MEMORY 0xb8000
+#define BLACK_ON_WHITE 0x0f
+void printf(char *);
+void entry()
+{
+	printf("Chordia is a gay boiiiii");
+
+	while(1){}
+}
+
+void printf(char* Message)
+{
+	char* ptr = (char*) VIDEO_MEMORY;
+	while(Message)
+	{
+		*ptr = *Message;
+		Message++;
+		ptr += 2;
+	}
+}
