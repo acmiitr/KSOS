836f7a7d5e68e52e7366bb7b6f26a2f011817516
rishichordia <rishichordia.1206@gmail.com>
2020-03-25 20:58:36 +0530

    Small change

--- kernel/c/dadio.c
+++ kernel/c/dadio.c
@@ -1,4 +1,4 @@
-#include<stdint.h>
+#include <stdint.h>
 #include "dadio.h"
 #include "keyboard.h"
 #include "hal.h"
@@ -11,53 +11,55 @@ static char current_color = 0xf8;
 
 void clear()
 {
-	char* cursor=(char*)VIDEO_MEMORY;
-	*cursor=' ';
-	cursor++;*cursor=current_color;cursor++;
-	while((uint32_t)(cursor-VIDEO_MEMORY)<2*ROW*COL)
-		{
-			*cursor=' ';cursor++;
-			*cursor=current_color;cursor++;
-		}
+	char *cursor = (char *)VIDEO_MEMORY;
+	*cursor = ' ';
+	cursor++;
+	*cursor = current_color;
+	cursor++;
+	while ((uint32_t)(cursor - VIDEO_MEMORY) < 2 * ROW * COL)
+	{
+		*cursor = ' ';
+		cursor++;
+		*cursor = current_color;
+		cursor++;
+	}
 	set_cursor(0);
 }
 
-void printf(char* Message)
+void printf(char *Message)
 {
 	uint32_t pointer = get_cursor();
 	pointer <<= 1;
-	char* vga_cursor = (char*) VIDEO_MEMORY;
+	char *vga_cursor = (char *)VIDEO_MEMORY;
 	vga_cursor += pointer;
-	while	(*Message)
+	while (*Message)
 	{
-		char c=*Message;
-		switch(c)
+		char c = *Message;
+		switch (c)
 		{
-		 case '\n':
-		 	pointer=(uint32_t)(vga_cursor-VIDEO_MEMORY);
-			if(!(pointer%(2*ROW)))
-				pointer++;
-		 	while(pointer%(2*ROW)){
-			pointer++;
-			if(pointer==2*ROW*COL)
-			{pointer=0;break;}
-			}	
-		 	Message++;
-		 	vga_cursor=(char *)(VIDEO_MEMORY+pointer);
+		case '\n':
+			pointer = (uint32_t)(vga_cursor - VIDEO_MEMORY);
 
-    			break;
-		case '\t':
-			pointer=(uint32_t)(vga_cursor-VIDEO_MEMORY);
-			pointer+=16;
-			vga_cursor=(char*)(VIDEO_MEMORY+pointer);
+			pointer -= (pointer % (2 * ROW));
+			pointer += (2 * ROW);
+
+			if (pointer == 2 * ROW * COL) //This is to control overflow
+				pointer = 0;
 			Message++;
-	       		break;
+			vga_cursor = (char *)(VIDEO_MEMORY + pointer);
+			break;
+		case '\t':
+			pointer = (uint32_t)(vga_cursor - VIDEO_MEMORY);
+			pointer += 16;
+			vga_cursor = (char *)(VIDEO_MEMORY + pointer);
+			Message++;
+			break;
 		default:
-			  *vga_cursor = *Message;
-               		 Message++;
-               		 vga_cursor ++;
-               		 *vga_cursor = current_color;
-               		 vga_cursor ++;
+			*vga_cursor = *Message;
+			Message++;
+			vga_cursor++;
+			*vga_cursor = current_color;
+			vga_cursor++;
 		}
 	}
 	vga_cursor -= VIDEO_MEMORY;
@@ -66,87 +68,90 @@ void printf(char* Message)
 	set_cursor(pointer);
 }
 
-
 void printhex(uint32_t input)
 {
-	char buffer[11];  //0x + 8 + null
+	char buffer[11]; //0x + 8 + null
 	uint8_t pointer = 9;
-	for(int i=0;i<8;i++)
+	for (int i = 0; i < 8; i++)
 	{
 		int c = input & 0xF;
-		if(c<10)
-			c+=0x30;
+		if (c < 10)
+			c += 0x30;
 		else
-			c+=55;
+			c += 55;
 		buffer[pointer] = (char)c;
-		pointer --;
-		input >>=4;
+		pointer--;
+		input >>= 4;
 	}
-	
-//This is to make the thing smaller
+
+	//This is to make the thing smaller
 	pointer = 2;
-	while((buffer[pointer] == '0')&& (pointer < 9)) pointer ++;
-	pointer--;buffer[pointer] = 'x';
-	pointer--;buffer[pointer] = '0';
+	while ((buffer[pointer] == '0') && (pointer < 9))
+		pointer++;
+	pointer--;
+	buffer[pointer] = 'x';
+	pointer--;
+	buffer[pointer] = '0';
 	printf(buffer + pointer);
 }
 
 void printint(uint32_t input)
 {
 	char buffer[11] = "0000000000";
-	for(int i=0;i<10;i++)
+	for (int i = 0; i < 10; i++)
 	{
-		int temp = input %10;
-		buffer[10-i] = (char)(temp + 0x30);
+		int temp = input % 10;
+		buffer[10 - i] = (char)(temp + 0x30);
 		input /= 10;
 	}
 	printf(buffer);
 }
 
-void putc (char x)
+void putc(char x)
 {
 	uint32_t pointer = get_cursor();
-	char* vga_cursor = (char*) VIDEO_MEMORY;
-	if(x == '\b')
+	char *vga_cursor = (char *)VIDEO_MEMORY;
+	if (x == '\b')
 	{
-		if(pointer)
+		if (pointer)
 		{
 			pointer--;
-			vga_cursor += (pointer<<1);
+			vga_cursor += (pointer << 1);
 			*vga_cursor = 0;
 			vga_cursor[1] = vga_cursor[3];
 			set_cursor(pointer);
 		}
 	}
-	else if(x == '\n')
+	else if (x == '\n')
 		printf("\n");
 	else
 	{
-		vga_cursor += (pointer<<1);
-		vga_cursor[1] = (char) current_color;
+		vga_cursor += (pointer << 1);
+		vga_cursor[1] = (char)current_color;
 		*vga_cursor = x;
-		set_cursor(pointer+1);
+		set_cursor(pointer + 1);
 	}
 }
 
-void set_fg_color (enum vga_color c)
+void set_fg_color(enum vga_color c)
 {
 	current_color &= 0xf0;
 	current_color |= c;
 }
 
-void set_bg_color (enum vga_color c)
+void set_bg_color(enum vga_color c)
 {
 	current_color &= 0x0f;
-	current_color |= (c<<4);
+	current_color |= (c << 4);
 }
 
 char get_monitor_char()
 {
-	while(1)
+	while (1)
 	{
 		wait_for_keyboard();
 		char x = get_latest_char();
-		if(x) return (x);
+		if (x)
+			return (x);
 	}
 }
