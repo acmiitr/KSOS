1a4ed61bdbe6e0a9ec0e98e6ba91e123df81edef
rishichordia <rishichordia.1206@gmail.com>
2020-04-08 00:50:48 +0530

    Initial Approach

--- kernel/driver/FAT12.c
+++ kernel/driver/FAT12.c
@@ -11,7 +11,7 @@
 #define bpbRootEntries 	 512
 #define bpbTotalSectors  2880
 #define bpbMedia  0xf0  
-#define bpbSectorsPerFAT  6     ;We need all this
+#define bpbSectorsPerFAT  6 
 #define Max_Name_Length 11
 
 static bool string_compare(char* str1, char* str2);
@@ -23,12 +23,16 @@ static uint32_t root_sect_address;
 static uint8_t fat_sect_no;
 static uint32_t fat_sect_address;
 
-static int file_size;
-static int starting_data_cluster_number;
+typedef struct __attribute__ ((__packed__)) file_Meta {
+	uint32_t file_size;
+    uint16_t starting_data_cluster_number;
+}file_Meta;
 
-uint32_t buffer_read_; 
-uint32_t fat_buffer_read;
-char* file_read;
+
+
+static file_Meta file ;
+uint32_t buffer_read_[]; 
+uint32_t fat_buffer_read[];
 
 void initialize_FAT(uint32_t data_sect,uint32_t root_sect, uint32_t fat_sect)
 {
@@ -58,14 +62,14 @@ bool FAT_find_file(char* filename)
         //--------------------------------------------
         if(string_compare(root_name,filename))
             {
-               starting_data_cluster_number=pointer[26]+pointer[27]>>8;//Considering the little endian encoding
-                file_size=pointer[28]+(pointer[29]>>8)+(pointer[30]>>16)+(pointer[31]>>24);//Considering the little endian encoding
+               file.starting_data_cluster_number=pointer[26]+pointer[27]<<8;//Considering the little endian encoding
+                file.file_size=pointer[28]+(pointer[29]<<8)+(pointer[30]<<24)+(pointer[31]<<16);//Considering the little endian encoding
                 return true;
             }
         pointer+=32;  
     }
-    starting_data_cluster_number=0;
-    file_size=0;
+    file.starting_data_cluster_number=0;
+    file.file_size=0;
     return false;
 }
 
@@ -75,22 +79,60 @@ void read_file(char* filename)
     if(!FAT_find_file(filename))
         monitor_puts("File not found!");
 
-    else//file found
+    else                     //If  file found
     {
-        uint32_t starting_data_cluster_address=fat_sect_address+(starting_data_cluster_number-2)*512;
-        uint32_t fat_sector_address= fat_sect_address+ (starting_data_cluster_number/256)*512;
+        uint32_t data_cluster_address=data_sect_address+(file.starting_data_cluster_number-2)*512;
+        uint32_t fat_sector_address= fat_sect_address;
         
-        char* file_read[file_size];
+        char* file_read[file.file_size];
 
-        read_sectors_ATA_PIO(buffer_read_,starting_data_cluster_address,1);
-        read_sectors_ATA_PIO(fat_buffer_read,fat_sector_address,1);
-
-        if(file_size<512)
-            {
-                uint8_t * pointer=(uint8_t*)buffer_read_;
-                for(int i=0;i<file_size;i++)
-                    file_read[i]=(char)pointer[i];
+        //read_sectors_ATA_PIO(buffer_read_,data_cluster_address,1);
+        read_sectors_ATA_PIO(fat_buffer_read,fat_sector_address,root_sect_no-fat_sect_no);
+        
+        uint16_t* pointer_to_fat=(uint16_t*)fat_buffer_read;
+        pointer_to_fat+= file.starting_data_cluster_number;
+        int count=0;
+        do
+        {
+            read_sectors_ATA_PIO(buffer_read_,data_cluster_address,1);
+            char* reading=(char*)buffer_read_;
+            int t=0;
+            while(t<512 && count<file.file_size)
+            {   
+                file_read[count]=*reading;
+                count++;t++;reading++;
             }
+            //Defining the next cluster number from FAT and next adress in the data sector
+            uint8_t next_cluster_number=(*pointer_to_fat)>>8;
+            data_cluster_address=data_sect_address+(next_cluster_number-2)*512;         //next_cluster_number-2  as first and second not there
+            
+            
+            if(*pointer_to_fat!=0xFFF8)                                               //update only if it is not the last 
+            {
+                pointer_to_fat=(uint16_t*)fat_buffer_read;
+                pointer_to_fat+=next_cluster_number;
+            }
+        }
+        while(*pointer_to_fat!=0xFFF8);
+
+        // if(file.file_size<512)
+        //     {
+        //         uint8_t * pointer=(uint8_t*)buffer_read_;
+        //         for(int i=0;i<file.file_size;i++)
+        //             file_read[i]=(char)pointer[i];
+        //     }
+        //     else
+        //     {
+        //         uint16_t *pointer=(uint16_t*)fat_buffer_read;
+        //         pointer+=file.starting_data_cluster_number%256;
+        //         uint8_t next_cluster_number=(*pointer)>>8;
+
+        //         while(*pointer!=0xFFF8)
+        //         {
+                    
+        //         }
+        //     }
+            
     }
 }
 
